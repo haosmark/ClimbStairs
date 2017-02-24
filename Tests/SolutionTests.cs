7a5e5ce4eb9226ec45022751b61eeaeab45c015b
Denis St <haosmark@gmail.com>
2017-02-24 15:27:26 -0500

    Add test cases for ClimStairs 4 and ClimStairs 6

---
+++ Tests/SolutionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClimbingStairs;
+
+namespace Tests
+{
+    [TestClass]
+    public class SolutionTests
+    {
+        [TestMethod]
+        public void Test_ClimbStairs_4()
+        {
+            // Arrange
+            int n = 4;
+            int expected = 5;
+            int actual;
+
+            // Act
+            actual = Solution.ClimbStairs(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_ClimbStairs_6()
+        {
+            // Arrange
+            int n = 6;
+            int expected = 13;
+            int actual;
+
+            // Act
+            actual = Solution.ClimbStairs(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
